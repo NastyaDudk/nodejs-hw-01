9ab681274e6854a6fc55ff8675fa4212042de110
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2024-05-22 16:05:12 +0400

    ok

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,24 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
-import fs from 'fs/promises';
+import fs from 'node:fs/promises';
 
 export const getAllContacts = async () => {
-    try {
-        const data = await fs.readFile(PATH_DB, 'utf8');
-        console.log('Вміст файлу:', data);
-        return data; 
-    } catch (err) {
-        console.error('Помилка при читанні файлу:', err);
-        throw err; 
-    }
+    const data = await fs.readFile(PATH_DB, 'utf8');
+
+    const parseContacts = JSON.parse(data);
+
+    return parseContacts;
 };
 
-async function main() {
-    try {
-        const contacts = await getAllContacts();
-        console.log(contacts);
-    } catch (error) {
-        console.error('Помилка у функції main:', error);
-    }
-}
-
-main();
+console.log(await getAllContacts());
